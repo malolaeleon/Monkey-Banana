03f1f0c42bb2ccbeb9ccb0bb75288e56082f8003
malolaeleon <malolaeleon@hunterschools.org>
2025-10-05 12:12:48 -0400

    Update bananabid.py

--- bananabid.py
+++ bananabid.py
@@ -2,12 +2,12 @@ def bananabid(my_player_number, my_bananas, monkey_position, opponent_bananas, p
 	
 	if turn_number == 1:
 		return 32
-	if opponent_bananas == 0
+	if opponent_bananas == 0:
 		return 1
 	last_turn = past_bid_list[-1]
 	opponent_bid = last_turn[2 - my_player_number]
-	my_bid = last_turn[my_player_number]
-	if my_bid <= opponent_bid 
+	my_bid = last_turn[my_player_number - 1]
+	if my_bid <= opponent_bid: 
 		return opponent_bid/2 + 1
 	
 	else:
